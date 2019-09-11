bb28baa71ceb72cb2b0cc03949df6e8078afea20
Fabian <fabianloaeza8@gmail.com>
2019-09-10 19:31:56 -0600

    grafics

--- resources/views/monitoring.blade.php
+++ resources/views/monitoring.blade.php
@@ -3,7 +3,25 @@
 
 @section('content')
 <head>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+ 
+<script src="https://cdn.jsdelivr.net/momentjs/2.14.1/moment.min.js"></script>
+ 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+ 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
+ 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css">
+ 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+<script>
+$(function () {
+  $('#example1').datetimepicker();
+});
+</script>
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
       google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
@@ -28,25 +46,29 @@
         chart.draw(data, options);
       }
     </script>
-  </head>
-<body>
-<div id="graphic"> 
-	<div class="container">
-		<div class="section-title">
-			<h2>Grafica</h2>
-			
-		</div>
-		<div class="row">
-			<div id="curve_chart" style="width: 900px; height: 500px"></div>
 
-			
-		</div>
-			
-		</div>
-		
-	</div>
-</div>		
-	
+
+
+</head>
+<body>
+
+<div class="intro">
+<div class="overlay">
+  <div class="container">
+    <div id="graphic">
+      <div class="section-title">
+      <h2>Grafica</h2>
+      
+      <div id="curve_chart" style="width: 900px; height: 500px"></div>
+      
+    </div>  
+    </div>
+    
+  </div>
+  
+</div>  
+</div>   
+  
 </body>
 
 
